f348230c28614a568068241a1f5a7d93017528b4
Strobel Pierre <strobelpierre@gmail.com>
2026-03-19 12:45:03 +0100

    ci: add commitlint for conventional commit enforcement

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2026 OIDC Groups Mapping Contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+};
